b2ca1a3d7b2017e93eb97cee14319cb7267cd123
simon-the-shark <simon_rekin@o2.pl>
2020-11-02 16:10:12 +0100

    changed DateField to DateTimeField

--- que/models.py
+++ que/models.py
@@ -1,10 +1,12 @@
 import jsonfield
-from django.conf import settings
 from django.db import models
 
 
 class PrincipalName(models.Model):
     name = models.EmailField()
+
+    def __str__(self):
+        return self.name
 
 
 class AuthorizedTeamsUser(models.Model):
@@ -22,10 +24,16 @@ class AuthorizedTeamsUser(models.Model):
         except:
             return False
 
+    def __str__(self):
+        return self.principal_name
+
 
 class QueueTicket(models.Model):
     user = models.ForeignKey(AuthorizedTeamsUser, on_delete=models.CASCADE)
-    in_queue_since = models.DateField(auto_now_add=True)
+    in_queue_since = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["in_queue_since"]
+
+    def __str__(self):
+        return self.user.display_name + " since " + str(self.in_queue_since)
