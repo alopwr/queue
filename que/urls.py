559afb8583038ffe202e22bebebe87eb727772a2
m4k5 <27677566+m4k5@users.noreply.github.com>
2020-11-03 10:08:48 +0100

    full queue support

--- que/urls.py
+++ que/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.QueueView.as_view(), name="que"),
     path("next", views.next_view, name="next"),
     path("clear", views.clear_view, name="clear"),
+    path("cancel", views.cancel_view, name="cancel"),
     path("login", views.sign_in, name="login"),
     path("logout", views.logout, name="logout"),
     path("callback", views.callback, name="callback"),
