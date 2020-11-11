e10488863e774f52378ae11ce04c4d0b9b559a6c
m4k5 <27677566+m4k5@users.noreply.github.com>
2020-11-11 17:58:00 +0100

    try/except because of missing request parameter

--- que/templatetags/teams_tab_tags.py
+++ que/templatetags/teams_tab_tags.py
@@ -6,4 +6,7 @@ register = template.Library()
 @register.simple_tag()
 @register.filter()
 def is_teams(request):
-    return "teams" in request.META["HTTP_USER_AGENT"].lower()
+    try:
+        return "teams" in request.META["HTTP_USER_AGENT"].lower()
+    except AttributeError:
+        return False
