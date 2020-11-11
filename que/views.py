b590937181bde84714b37aa6615662d36a72c3a1
m4k5 <27677566+m4k5@users.noreply.github.com>
2020-11-11 18:24:03 +0100

    fix of missing context param

--- que/views.py
+++ que/views.py
@@ -173,6 +173,7 @@ class StudentNotInQueueView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["queue_length"] = max(QueueTicket.objects.count(), 0)
         context["estimated_time"] = context["queue_length"] * average_meeting_time() + 1
         return context
 
