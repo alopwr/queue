4b857597ad58c7fe7d9172db6806d86e39139d8f
m4k5 <27677566+m4k5@users.noreply.github.com>
2020-11-24 09:58:26 +0100

    implementation of Sentry error monitoring

--- queueapp/settings.py
+++ queueapp/settings.py
@@ -33,7 +33,18 @@ ALLOWED_HOSTS = [
     "queue.skica.dev",
     "queue.kowalinski.dev",
 ]
+if not DEBUG and config("SENTRY_DSN"):
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
 
+    sentry_sdk.init(
+        dsn=config("SENTRY_DSN"),
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
 # Application definition
 
 INSTALLED_APPS = [
