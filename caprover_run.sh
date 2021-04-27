c8981ba94ca649bd66df8dd194d5dd2f3de2df80
m4k5 <27677566+m4k5@users.noreply.github.com>
2021-04-27 12:11:03 +0200

    new binding

--- caprover_run.sh
+++ caprover_run.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker queueapp.asgi:application
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker queueapp.asgi:application --bind=0.0.0.0:80
